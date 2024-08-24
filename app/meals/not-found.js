d09439f75ce5b700f4687601190c20628c2cde33
BehnoodAmini <behnooda81@gmail.com>
2024-08-24 15:35:22 +0330

    #

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h2>404 MEAL NOT FOUND</h2>
+            <p>This meal does not exist.</p>
+        </main>
+    )
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h2>404 NOT FOUND</h2>
+            <p>This page does not exist.</p>
+        </main>
+    )
+}
