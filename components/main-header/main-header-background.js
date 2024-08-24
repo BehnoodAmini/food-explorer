0e08f27299fa461b8ae66dfc0757203887154e88
BehnoodAmini <behnooda81@gmail.com>
2024-08-24 15:56:48 +0330

    #

--- components/main-header/main-header-background.js
+++ components/main-header/main-header-background.js
@@ -1,6 +1,6 @@
 import classes from "./main-header-background.module.css"
 
-export default function () {
+export default function MainHeaderBackground() {
     return (
         <div className={classes['header-background']}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
